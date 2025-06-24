effb0e9f7beb2baa78de3ab7f935c1e3bf5d081d
Alexander DeJesus <alexdejesus336@yahoo.com>
2025-06-24 05:33:16 -0400

    Added I/O management

--- main.go
+++ main.go
@@ -4,7 +4,31 @@ import "fmt"
 import "os"
 import "os/exec"
 import "strings"
+import "encoding/json"
+import "bufio"
+import "strconv"
 
+func loadContacts(filename string) []Contact {
+	file, err := os.Open(filename)
+	if err != nil {
+		return []Contact{}
+	}
+	defer file.Close()
+
+	var contacts []Contact
+	json.NewDecoder(file).Decode(&contacts)
+	return contacts
+}
+
+func saveContacts(filename string, contacts []Contact) {
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error saving contacts:", err)
+		return
+	}
+	defer file.Close()
+	json.NewEncoder(file).Encode(contacts)
+}
 
 func clearScreen(){
 	cmd := exec.Command("clear")
@@ -14,31 +38,45 @@ func clearScreen(){
 
 func main(){
 	
-	var contacts [] Contact
+	file := "contacts.json"
+	var contacts [] Contact = loadContacts(file)
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Println("1) Add Contact")
 		fmt.Println("2) List Contacts")
 		fmt.Println("3) Search Contact")
-		fmt.Println("4) Exit\n")
+		fmt.Println("4) Delete Contact")
+		fmt.Println("5) Exit\n")
 		fmt.Print("Choose an option: ")
 		
 
-		var choice int
-		fmt.Scanln(&choice)
+		choiceStr, _ := reader.ReadString('\n')
+		choiceStr = strings.TrimSpace(choiceStr)
+		choice, err := strconv.Atoi(choiceStr)
+
+		if err != nil {
+            fmt.Println("Please enter a valid number.")
+            fmt.Println("Press enter to continue...")
+            reader.ReadString('\n')
+            clearScreen()
+            continue
+        }
 
 		switch choice {
 
 			case 1:
-				var name, phone string
 				fmt.Print("Enter name: ")
-				fmt.Scanln(&name)
+				name, _ := reader.ReadString('\n')
+				name = strings.TrimSpace(name)
 
 				fmt.Print("Enter phone: ")
-				fmt.Scanln(&phone)
+				phone, _ := reader.ReadString('\n')
+				phone = strings.TrimSpace(phone)
 
 				newContact := Contact{Name: name, Phone: phone}
 				contacts = append(contacts, newContact)
+				saveContacts(file, contacts)
 				fmt.Println("Contact added.\n")
 			case 2:
 				if len(contacts) == 0 {
@@ -50,9 +88,9 @@ func main(){
 					fmt.Print("\n")
 				}
 			case 3:
-				var searchName string
 				fmt.Print("Enter name to search: ")
-				fmt.Scanln(&searchName)
+				searchName, _ := reader.ReadString('\n')
+				searchName = strings.TrimSpace(searchName)
 				found := false
 
 				for _, contact := range contacts {
@@ -66,6 +104,27 @@ func main(){
 				}
 				fmt.Print("\n")
 			case 4:
+				if len(contacts) == 0 {
+					fmt.Println("No contacts to delete\n")
+				} else {
+					for index, contact := range contacts {
+						fmt.Printf("%d. Name: %s, Phone: %s\n", index + 1, contact.Name, contact.Phone)
+					}
+					fmt.Print("Enter the number of the contact to delete: ")
+					delStr, _ := reader.ReadString('\n')
+					delStr = strings.TrimSpace(delStr)
+					delIndex, err := strconv.Atoi(delStr)
+					
+					if err != nil || delIndex < 1 || delIndex > len(contacts) {
+						fmt.Println("Invalid contact number.\n")
+					} else {
+						contacts = append(contacts[:delIndex - 1], contacts[delIndex:]...)
+						saveContacts(file, contacts)
+
+						fmt.Println("Contact deleted.\n")
+					}
+				}
+			case 5:
 				fmt.Println("Goodbye!")
 				return
 			default:
@@ -76,7 +135,7 @@ func main(){
 		}
 
 		fmt.Println("Press enter to continue...")
-		fmt.Scanln()
-		clearScreen()
+        reader.ReadString('\n')
+        clearScreen()
 	}
 }
